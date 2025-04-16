bd5a745f82fd1ad14073d10a60839133a317fe14
s.kobashigawa <hip002383@gmail.com>
2025-04-16 16:28:23 +0900

    'add'

---
+++ sub.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "message.h"
+
+void printMessage() {
+    printf("Hello, World!\n");
+}
---
+++ sub.h
@@ -0,0 +1,6 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+void printMessage();
+
+#endif
